64a6a385cd4996c5b193dd3c406507f41c903638
Rubén Perea <perea7r@gmail.com>
2023-01-16 11:07:13 +0100

    include tailwind and daysiui

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    daisyui: {
+        themes: [
+          {
+            mytheme: {
+            "primary": "#e5d352",
+            "secondary": "#4b7f52",
+            "accent": "#2e5077",
+            "neutral": "#c5d1eb",
+            "base-100": "#efe9f4",
+            "info": "#89D7E6",
+            "success": "#32DC98",
+            "warning": "#F9D348",
+            "error": "#F27388",
+            },
+          },
+        ],
+      },
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
