d64963080410d9f461a9b780c926058e0377646b
Rubén Perea <92312967+Perea7r@users.noreply.github.com>
2023-06-07 16:28:26 +0200

    ring photo

--- resources/views/partials/fotoPerfil.blade.php
+++ resources/views/partials/fotoPerfil.blade.php
@@ -1,3 +1,4 @@
-<div tabindex="0" class="btn btn-circle btn-ghost w-16 h-16 !outline-none focus:btn-ghost">
+<div tabindex="0"
+    class="btn hover:bg-transparent btn-circle focus:ring focus:ring-secondary hover:ring hover:ring-primary w-16 h-16 bg-transparent">
     <img src="/images/user.png" alt="Usuario" />
 </div>
