7407821d6e822324c817f6b36ebad45bbea24812
Rubén Perea <92312967+Perea7r@users.noreply.github.com>
2023-06-12 13:40:56 +0200

    register

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -25,55 +25,38 @@
 
     @yield('content')
 
-    <div class="flex h-screen flex-col md:flex-row items-center justify-center bg-gray-100">
-        <div class="flex h-screen flex-col md:flex-row items-center justify-center bg-gray-100">
-            <div class="flex-grow-1 flex flex-col items-center justify-center my-auto">
-                <div
-                    class="w-full max-w-md bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 font-Montserrat">
-                    <div class="space-y-4 md:space-y-6 sm:p-8">
-                        <div class="flex items-center justify-center">
-                            @include('partials.logo.logo', ['classes' => 'w-12 h-12'])
-                        </div>
-                        <h1
-                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Crear cuenta</h1>
-                        <form class="space-y-6" action="#">
-                            <div>
-                                <label for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                    Email</label>
-                                <input type="email" name="email" id="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="tu_email@email.com" required="">
-                            </div>
-                            <div>
-                                <label for="password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required="">
-                            </div>
-                            <div>
-                                <label for="confirm-password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar
-                                    contraseña</label>
-                                <input type="password" name="confirm-password" id="confirm-password"
-                                    placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required="">
-                            </div>
-                            <div>
-                                <button type="submit"
-                                    class="w-full bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-base px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary">Crea
-                                    tu cuenta</button>
-                        </form>
-                    </div>
-                </div>
+    <section class="h-screen w-screen flex items-center justify-center bg-slate-100">
+
+        <!-- Left column container with background-->
+        <div class="w-3/4 shadow-xl rounded-md  gap-20 flex flex-row items-center justify-center bg-slate-50">
+            <div>
+                @include('partials.logo.logo')
             </div>
+
+            <!-- Right column container -->
+
+            <form class="my-8 w-60 flex flex-col justify-center">
+                <h2 class="font-Montserrat text-2xl font-bold my-8">Nuevo usuario</h2>
+
+                <div class="form-control gap-4">
+                    <input placeholder="Nombre*" class="text__input" type="text" name="nombre" id="nombre">
+                    <input placeholder="Apellidos*" class="text__input" type="text" name="apellidos" id="apellidos">
+                    <input placeholder="Email*" class="text__input" type="email" name="email" id="email">
+                    <input placeholder="Contraseña*" class="text__input" type="password" name="password" id="password">
+                </div>
+
+
+
+                <button
+                    class="btn rounded-none btn-block bg-gradient-to-r border-black from-secondary to-green-400 capitalize text-base font-Montserrat my-8">Registrarme</button>
+
+            </form>
         </div>
 
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </section>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </body>
 
