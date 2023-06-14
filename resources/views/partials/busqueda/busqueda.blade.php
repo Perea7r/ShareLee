210b002f5118d894d405444767a60524e8a1acd9
Rubén Perea <perea7r@gmail.com>
2023-06-14 21:58:04 +0200

    filter

--- resources/views/partials/busqueda/busqueda.blade.php
+++ resources/views/partials/busqueda/busqueda.blade.php
@@ -6,7 +6,7 @@
                     class="input join-item !outline-none border-b-2" placeholder="Busca tu libro..." />
                     <ul tabindex="0" class="dropdown-content menu bg-base-100 z-50">
                         @foreach ($libros as $libro)
-                            <li> <a href="">{{ $libro->titulo }}</a></li>
+                            <li> <a href="/ejemplar/{{ $libro->idEjemplar }}">{{ $libro->titulo }}</a></li>
                         @endforeach
                     </ul>
             </div>
@@ -15,3 +15,28 @@
     </div>
 
 </form>
+
+<script>
+    const libros = {!! json_encode($libros) !!}; // Obtener los libros desde Laravel
+
+const searchInput = document.querySelector('input[name="search"]');
+const list = document.querySelector('.dropdown-content');
+
+searchInput.addEventListener('input', () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredLibros = libros.filter(libro =>
+    libro.titulo.toLowerCase().includes(searchTerm)
+  );
+
+  list.innerHTML = ''; // Limpiar la lista
+
+  filteredLibros.forEach(libro => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = `/ejemplar/${libro.idEjemplar}`;
+    link.textContent = libro.titulo;
+    listItem.appendChild(link);
+    list.appendChild(listItem);
+  });
+});
+</script>
