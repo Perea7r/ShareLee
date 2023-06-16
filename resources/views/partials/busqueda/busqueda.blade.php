873d1d6aa77e8aafdd38d7b409b00061cacb2a5a
Rubén Perea <perea7r@gmail.com>
2023-06-16 21:20:21 +0200

    filter reduce 5

--- resources/views/partials/busqueda/busqueda.blade.php
+++ resources/views/partials/busqueda/busqueda.blade.php
@@ -4,19 +4,28 @@
             <div class="dropdown dropdown-bottom">
                 <input tabindex="0" type="search" name="search" value="{{ $search }}" autocomplete="off"
                     class="input join-item !outline-none border-b-2" placeholder="Busca tu libro..." />
-                    <ul tabindex="0" class="dropdown-content menu bg-base-100 z-50">
-                        @foreach ($libros as $libro)
-                            @if ($loop->index < 5)
-                                <li> <a href="/ejemplar/{{ $libro->idEjemplar }}">{{ $libro->titulo }}</a></li>
-                            @endif
-                        @endforeach
-                    </ul>
+                <ul tabindex="0" class="dropdown-content menu bg-base-100 z-50">
+                    @php
+                        $count = 0;
+                    @endphp
+                    @foreach ($libros as $libro)
+                        @if ($loop->index < 5 && stripos($libro->titulo, request('search')) !== false)
+                            <li><a href="/ejemplar/{{ $libro->idEjemplar }}">{{ $libro->titulo }}</a></li>
+                            @php
+                                $count++;
+                            @endphp
+                        @elseif ($count >= 5)
+                            @break
+                        @endif
+                    @endforeach
+                </ul>
             </div>
         </div>
         <button class="join-item btn-primary w-20">Search</button>
     </div>
 
 </form>
+
 
 <script>
     const libros = {!! json_encode($libros) !!}; // Obtener los libros desde Laravel
@@ -32,7 +41,7 @@
 
         list.innerHTML = ''; // Limpiar la lista
 
-        filteredLibros.forEach(libro => {
+        filteredLibros.slice(0, 5).forEach(libro => {
             const listItem = document.createElement('li');
             const link = document.createElement('a');
             link.href = `/ejemplar/${libro.idEjemplar}`;
