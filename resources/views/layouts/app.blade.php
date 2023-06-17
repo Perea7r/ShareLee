af2dc7641cb4542fa7b88b9abe74b16286253f81
Rubén Perea <perea7r@gmail.com>
2023-06-17 16:42:35 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ShareLee - @yield('title')</title>
+    <link rel="icon" href="images/Logo.png">
     <meta name="description" content="@yield('meta-description', 'default meta description')">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
