564e4499f9e770dafd45e39ebf893bb33afea386
Rubén Perea <perea7r@gmail.com>
2023-06-17 22:18:15 +0200

    nav

---
+++ resources/views/partials/nav/navAprendizaje.blade.php
@@ -0,0 +1,19 @@
+<nav class="canvasOff z-10 flex text-white/100 p-10 bg-[#38003eff]/50 justify-center font-Scifi">
+    <ul class="flex gap-10">
+        <li class="text-lg font-medium">
+            <a href="#" class="hover:underline hover:underline-offset-8 hover:text-accent text">SOBRE MÍ</a>
+        </li>
+        <li class="text-lg font-medium">
+            <a href="#projects" class="hover:underline hover:underline-offset-8 hover:text-accent">CONOCIMIENTOS</a>
+        </li>
+        <li class="text-lg font-medium">
+            <a href="#about" class="hover:underline hover:underline-offset-8 hover:text-accent">PORTFOLIO</a>
+        </li>
+        <li class="text-lg font-medium">
+            <a href="#about" class="hover:underline hover:underline-offset-8 hover:text-accent">CERTIFICADOS</a>
+        </li>
+        <li class="text-lg font-medium">
+            <a href="#about" class="hover:underline hover:underline-offset-8 hover:text-accent">CONTÁCTAME</a>
+        </li>
+    </ul>
+</nav>
