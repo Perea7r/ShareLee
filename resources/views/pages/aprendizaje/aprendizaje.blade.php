a57fb7fd28481c55ee4bee7033eeb8a461572976
Rubén Perea <perea7r@gmail.com>
2023-06-19 01:49:06 +0200

    tarjetas

--- resources/views/pages/aprendizaje/aprendizaje.blade.php
+++ resources/views/pages/aprendizaje/aprendizaje.blade.php
@@ -39,7 +39,7 @@
         <h1 data-aos="fade-up" class="font-Scifi text-4xl">Sobre mi</h1>
 
         <div class="flex relative flex-direction-row justify-center items-center gap-7 mt-10 top-1">
-            <img src="/images/ruben.png" alt="Ruben Perea">
+            <img class="w-60" src="/images/ruben.png" alt="Ruben Perea">
             <a class="a w-36 text-center" href="/pdf/ruben.pdf" target="_blank">
                 <span></span>
                 <span></span>
@@ -50,35 +50,39 @@
         </div>
 
         <div class="max-w-sm w-full lg:max-w-full lg:flex">
-            <div class="flex-1 lg:flex-none h-48 bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                style="background-image: url('/images/cosmere.jpg')" title="Woman holding a mug"></div>
             <div
-                class="flex-1 lg:border-r lg:border-b lg:border-l lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                    <p class="text-sm text-gray-600 flex items-center"></p>
-                    <div class="text-gray-900 font-bold text-xl mb-2">¿Quién soy?</div>
-                    <p class="text-gray-700 text-base">Soy Rúben, tengo 28 años y soy de Sevilla, España. Fui cocinero y trabajé durante 5 años. Sin embargo, mi pasión se encuentra en el mundo del desarrollo, con un gran interés en el diseño. En 2021 comencé mi camino como desarrollador, enfocándome en expandir mis habilidades tecnológicas y mejorar mis conocimientos en busca de una carrera profesional. </p>
-                </div>d
-            </div>
-            <div class="flex-1 lg:flex-none h-48 bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                style="background-image: url('/images/cosmere.jpg')" title="Woman holding a mug"></div>
-            <div
-                class="flex-1 lg:border-r lg:border-b lg:border-l lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                    <p class="text-sm text-gray-600 flex items-center"></p>
-                    <div class="text-gray-900 font-bold text-xl mb-2">¿Cúales son mis metas?</div>
-                    <p class="text-gray-700 text-base">Mi objetivo es ser un todoterreno en el mundo del desarrollo, busco proyectos desafiantes u oportunidades laborales donde pueda aplicar de manera efectiva mis habilidades en programación y diseño, contribuyendo a la creación de experiencias digitales,innovadoras y amigables para los usuarios</p>
+                class="flex-1 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal" style="background-image: linear-gradient(to bottom, #4b006e, #0b0051);">
+                <div class="">
+                    <p class="text-sm flex items-center"></p>
+                    <div class="font-Scifi text-accent font-bold text-xl mb-2">¿Quién soy?</div>
+                    <p class="font-Montserrat text-lg">Tengo 28 años y soy de Sevilla, España. Fui cocinero y
+                        trabajé durante 5 años. Sin embargo, mi pasión se encuentra en el mundo del desarrollo, con un
+                        gran interés por el diseño. En 2021 comencé mi camino como desarrollador, enfocándome en expandir
+                        mis habilidades tecnológicas y mejorar mis conocimientos en busca de una carrera profesional.
+                    </p>
                 </div>
             </div>
-            <div class="flex-1 lg:flex-none h-48 bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                style="background-image: url('/images/cosmere.jpg')" title="Woman holding a mug"></div>
+
             <div
-                class="flex-1 lg:border-r lg:border-b lg:border-l lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                    <p class="text-sm text-gray-600 flex items-center"></p>
-                    <div class="text-gray-900 font-bold text-xl mb-2">¡Conóceme un poco más!
+                class="flex-1 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"  style="background-image: linear-gradient(to bottom, #FFB900, #FF773D);">
+                <div class="" >
+                    <p class="text-sm flex items-center"></p>
+                    <div class="font-Scifi text-black font-bold text-xl mb-2">¿Cúales son mis metas?</div>
+                    <p class="font-Montserrat text-black text-lg">Mi objetivo es ser un todoterreno en el mundo del desarrollo,
+                        busco proyectos desafiantes u oportunidades laborales donde pueda aplicar de manera efectiva mis
+                        habilidades en programación y diseño, contribuyendo a la creación de experiencias
+                        digitales,innovadoras y amigables para los usuarios</p>
+                </div>
+            </div>
+            <div
+                class= "flex-1 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal" style="background-image: linear-gradient(to bottom, #00FFFF, #0080FF);">
+                <div class="">
+                    <p class="text-sm flex items-center"></p>
+                    <div class="font-Scifi text-black font-bold text-xl mb-2">¡Conóceme un poco más!
                     </div>
-                    <p class="text-gray-700 text-base">En mi tiempo libre me encanta jugar a videojuegos, hacer deporte y conocer nuevos lugares y personas. Tengo dos gatos Daiki y Nicole, que les encanta apoyarse en mi teclado y borrar mi código</p>
+                    <p class="font-Montserrat text-black text-lg">En mi tiempo libre me encanta jugar a videojuegos, hacer deporte
+                        y conocer nuevos lugares y personas. Tengo dos gatos Daiki y Nicole, que les encanta apoyarse en
+                        mi teclado y borrar mi código</p>
                 </div>
             </div>
         </div>
