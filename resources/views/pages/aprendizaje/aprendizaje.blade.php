80d98c6aa35b415867acd5e3243a5ebea6470929
Rubén Perea <perea7r@gmail.com>
2023-06-19 02:53:11 +0200

    buttons

--- resources/views/pages/aprendizaje/aprendizaje.blade.php
+++ resources/views/pages/aprendizaje/aprendizaje.blade.php
@@ -42,100 +42,112 @@
         <h1 data-aos="fade-up" class="mt-10 font-Scifi text-4xl">Tecnologías</h1>
 
         <div class="grid grid-cols-4 gap-4">
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #d84924;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/html.svg" alt="HTML">
+              <p>HTML</p>
             </div>
           
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000"  class="a w-36 text-center" style="--buttoncolor: #254bdd;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/css.svg" alt="CSS">
+              <p>CSS</p>
             </div>
           
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #ead41c;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/js.svg" alt="JS">
+              <p>JS</p>
             </div>
           
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #c36291;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/sass.svg" alt="SASS">
+              <p>SASS</p>
             </div>
           
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #f22a1d;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/laravel.svg" alt="LARAVEL">
+              <p>LARAVEL</p>
             </div>
           
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #7175aa;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/php.svg" alt="PHP">
+              <p>PHP</p>
             </div>
 
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #67aa3c;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/spring.svg" alt="SPRING">
+              <p>SPRING</p>
             </div>
 
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #e01e23;">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p class="font-Scifi">CV</p>
+              <img class="invert" src="/images/skills/java.svg" alt="JAVA">
+              <p>JAVA</p>
             </div>
 
-            <div class="a w-36 text-center">
+            <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #3b7f3a;">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                <p class="font-Scifi">CV</p>
+                <img class="invert" src="/images/skills/node.svg" alt="NODEJS">
+              <p>NODEJS</p>
               </div>
             
-              <div class="a w-36 text-center">
+              <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #07adca;">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                <p class="font-Scifi">CV</p>
+                <img class="invert" src="/images/skills/tailwind.svg" alt="TAILWIND">
+              <p>TAILWIND</p>
               </div>
   
-              <div class="a w-36 text-center">
+              <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #d78620;">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                <p class="font-Scifi">CV</p>
+                <img class="invert" src="/images/skills/mysql.svg" alt="MYSQL">
+              <p>MYSQL</p>
               </div>
   
-              <div class="a w-36 text-center">
+              <div data-aos="zoom-in" data-aos-duration="1000" class="a w-36 text-center" style="--buttoncolor: #bf4231;">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                <p class="font-Scifi">CV</p>
+                <img class="invert" src="/images/skills/oracle.svg" alt="ORACLE">
+              <p>ORACLE</p>
               </div>
             
           </div>
